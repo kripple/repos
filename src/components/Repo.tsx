e49eae264caa0647c1e50b27a60f9310cde661e8
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-01-08 14:36:48 -0500

    add repo details

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -7,8 +7,19 @@ import { SvgIcon } from '@/components/SvgIcon';
 import '@/components/repo.css';
 
 export function Repo({
-  data: { name },
-  // data: { name, ...repo },
+  data: {
+    name,
+    html_url,
+    description,
+    created_at,
+    updated_at,
+    homepage,
+    size,
+    language,
+    has_pages,
+    license,
+    default_branch,
+  },
   hide,
   highlight,
   selected,
@@ -24,11 +35,6 @@ export function Repo({
 }) {
   // const { currentData } = useRepo(name);
 
-  // const [selected, setSelected] = useState<string>();
-  //   const selectRepo = (event) => {
-  //     setSelectedRepo()
-  //   }
-
   const __html = highlight
     ? name.replace(
         highlight,
@@ -41,11 +47,77 @@ export function Repo({
       <option
         className="repo-contents"
         dangerouslySetInnerHTML={{ __html }}
-        onClick={select as unknown as MouseEventHandler<HTMLOptionElement>} // ignore unhelpful type definitions
+        onClick={select as unknown as MouseEventHandler<HTMLOptionElement>}
         role={selected ? undefined : 'button'}
         style={hide ? { display: 'none' } : undefined}
         value={name}
       ></option>
+      <div className="repo-details">
+        {description ? (
+          <div>
+            <span className="label">Description: </span>
+            {description}
+          </div>
+        ) : null}
+
+        {language ? (
+          <div>
+            <span className="label">Language: </span>
+            {language}
+          </div>
+        ) : null}
+
+        {license ? (
+          <a href={license.url} rel="noreferrer" tabIndex={0} target="_blank">
+            <span className="label">License: </span>
+            {license.name}
+          </a>
+        ) : null}
+
+        {has_pages ? (
+          <a
+            href={`https://kellyripple.com/${name}`}
+            rel="noreferrer"
+            tabIndex={0}
+            target="_blank"
+          >
+            <span className="label">Pages Site: </span>
+            {`kellyripple.com/${name}`}
+          </a>
+        ) : null}
+
+        {homepage ? (
+          <a href={homepage} rel="noreferrer" tabIndex={0} target="_blank">
+            <span className="label">Website: </span>
+            {homepage.replace('https://', '')}
+          </a>
+        ) : null}
+
+        <div>
+          <span className="label">Default Branch: </span>
+          {default_branch}
+        </div>
+
+        <div>
+          <span className="label">Size: </span>
+          {size}
+        </div>
+
+        <div>
+          <span className="label">Created On: </span>
+          {created_at}
+        </div>
+
+        <div>
+          <span className="label">Last Updated: </span>
+          {updated_at}
+        </div>
+
+        <a href={html_url} rel="noreferrer" tabIndex={0} target="_blank">
+          <SvgIcon icon="octocat" />
+          <span className="label">View on Github</span>
+        </a>
+      </div>
       <button
         className="close-button"
         onClick={close as unknown as MouseEventHandler<HTMLButtonElement>}
