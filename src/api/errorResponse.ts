9e80c99ffefb2b5b6d5fedd850fa41864289fce3
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-01-10 12:04:19 -0500

    lint

--- src/api/errorResponse.ts
+++ src/api/errorResponse.ts
@@ -1,19 +1,21 @@
 import { HttpResponse } from 'msw';
 
 export const ErrorResponse = {
-  RateLimit: () => HttpResponse.json(
-    { error: 'Forbidden' },
-    {
-      status: 403,
-      headers: {
-        'x-ratelimit-remaining': '0',
+  RateLimit: () =>
+    HttpResponse.json(
+      { error: 'Forbidden' },
+      {
+        status: 403,
+        headers: {
+          'x-ratelimit-remaining': '0',
+        },
       },
-    },
-  ),
-  NotFound: () => HttpResponse.json(
-    { error: 'Not Found' },
-    {
-      status: 404,
-    },
-  ),
+    ),
+  NotFound: () =>
+    HttpResponse.json(
+      { error: 'Not Found' },
+      {
+        status: 404,
+      },
+    ),
 } as const;
