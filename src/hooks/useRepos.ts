8422dd20cdc8ad236b7dbb624d1c945f574fbce7
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-01-10 15:26:55 -0500

    use singlepage request

--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -3,7 +3,26 @@ import { useEffect, useState } from 'react';
 import { config } from '@/api/config';
 import { api } from '@/api/endpoints';
 
+// because the rate limit is so strict,
+// we want to make one longer request instead of many faster ones
 export const useRepos = ({ itemsMax }: { itemsMax?: number }) => {
+  return api.useGetReposQuery(
+    {
+      itemsPerPage: itemsMax as number, // itemsMax is a skip param
+      page: 1,
+      username: config.username,
+    },
+    {
+      selectFromResult: ({ currentData, isFetching }) => ({
+        currentData,
+        isFetching,
+      }),
+      skip: !itemsMax,
+    },
+  );
+};
+
+export const useRepoPages = ({ itemsMax }: { itemsMax?: number }) => {
   const [page, setPage] = useState<number>(1);
 
   const response = api.useGetReposQuery(
