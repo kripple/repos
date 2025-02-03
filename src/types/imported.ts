08581f340e5244beb480267c17c5a01ba9794996
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-03 09:16:41 -0500

    declare react types

--- src/types/imported.ts
+++ src/types/imported.ts
@@ -1,12 +1,15 @@
 import type {
+  ChangeEvent as ChangeEventType,
   Dispatch,
   MouseEvent,
-  ChangeEvent as ReactChangeEvent,
+  ReactNode as ReactNodeType,
   SetStateAction,
 } from 'react';
 
-export type ChangeEvent = ReactChangeEvent<HTMLInputElement>;
+export type ChangeEvent = ChangeEventType<HTMLInputElement>;
 
 export type ClickEvent = MouseEvent<HTMLButtonElement>;
+
+export type ReactNode = ReactNodeType;
 
 export type SetState<T> = Dispatch<SetStateAction<T>>;
--- src/types/react.d.ts
+++ src/types/react.d.ts
@@ -4,4 +4,6 @@ declare type ChangeEvent = import('@/types/imported').ChangeEvent;
 
 declare type ClickEvent = import('@/types/imported').ClickEvent;
 
+declare type ReactNode = import('@/types/imported').ReactNode;
+
 declare type SetState<T> = import('@/types/imported').SetState<T>;
