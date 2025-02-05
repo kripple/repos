b965c77286718b0ef8e37f05a03041b1c8dd5cc8
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-05 16:21:51 -0500

    add profile fallback

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,6 +1,31 @@
+import type { FetchBaseQueryError } from '@reduxjs/toolkit/query/react';
+import { useEffect, useState } from 'react';
+
 import { config } from '@/api/config';
 import { api } from '@/api/endpoints';
+import { isQueryError } from '@/api/type-guards';
 
 export const useProfile = () => {
-  return api.useGetProfileQuery({ username: config.username });
+  const params = { username: config.username } as const;
+  const [errorStatus, setErrorStatus] =
+    useState<FetchBaseQueryError['status']>();
+  const isRateLimitError = errorStatus === 403 || errorStatus === 429;
+  const response = api.useGetProfileQuery(params);
+  const [triggerFallback, fallbackResponse] =
+    api.useLazyGetProfileFallbackQuery();
+
+  useEffect(() => {
+    if (!isQueryError(response.error)) return;
+    const responseStatus = response.error.status;
+    setErrorStatus((current) =>
+      current === responseStatus ? current : responseStatus,
+    );
+  }, [response.error]);
+
+  useEffect(() => {
+    if (!isRateLimitError) return;
+    triggerFallback(params);
+  }, [isRateLimitError]);
+
+  return isRateLimitError ? fallbackResponse : response;
 };
