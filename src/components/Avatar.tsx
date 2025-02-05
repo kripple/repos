6f8433bb30117a13cfd6c6fb2cac4cb60ba24eac
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-04 20:41:24 -0500

    add fallback avatar image

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import { useCallback, useState } from 'react';
 
+import fallbackImage from '@/assets/avatar.png';
 import { useProfile } from '@/hooks/useProfile';
 
 import '@/components/avatar.css';
@@ -8,9 +9,12 @@ import '@/components/avatar.css';
 export function Avatar() {
   const { currentData, isLoading } = useProfile();
   const avatarUrl = currentData?.avatar_url;
+  const url = !isLoading && !avatarUrl ? fallbackImage : avatarUrl;
 
   const [lastShimmer, setLastShimmer] = useState<boolean>(false);
-  const loaded = Boolean(!isLoading && avatarUrl && lastShimmer);
+  const loaded = Boolean(!isLoading && url && lastShimmer);
+
+  const backgroundImage = loaded ? `url(${url})` : undefined;
 
   const unveil = useCallback(() => {
     setLastShimmer(true);
@@ -19,12 +23,7 @@ export function Avatar() {
   return (
     <div className="avatar" data-loaded={!isLoading} data-testid="Avatar">
       <div className="circle-crop fancy-hover-effect">
-        <div
-          className="image"
-          style={{
-            backgroundImage: loaded ? `url(${avatarUrl})` : undefined,
-          }}
-        >
+        <div className="image" style={{ backgroundImage }}>
           <div
             className={classNames('avatar-shimmer', { loaded })}
             data-testid="AvatarShimmer"
