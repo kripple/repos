9d824074ac540d8417ff4b3630ef3880aba69ec1
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-03-15 18:40:47 -0400

    remove unused

--- src/utils/format.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (string: string) =>
-  new Date(string).toLocaleString('en-US', {
-    year: 'numeric',
-    month: 'long',
-  });
-
-export const formatDateTime = (string: string) => string.substring(0, 10);
